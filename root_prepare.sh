8962ef12d2f67296742c797e3c799869466f34ca
Luzi82 <luzi82@gmail.com>
2014-08-02 14:49:00 +0800

    auto confirm apt get

--- root_prepare.sh
+++ root_prepare.sh
@@ -1,10 +1,11 @@
 #/bin/bash
 
 # install required file
-sudo apt-get install git subversion build-essential
+sudo apt-get install git subversion build-essential -y
 
 # http://stackoverflow.com/questions/12591629/gcc-cannot-find-bits-predefs-h-on-i686
-sudo apt-get install gcc-multilib
+sudo apt-get install gcc-multilib -y
 
 # http://code.google.com/p/chromium/wiki/AndroidBuildInstructions
-sudo apt-get install g++-multilib
+sudo apt-get install g++-multilib -y
+
