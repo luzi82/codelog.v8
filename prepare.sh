5962166c0307c0543b36b05b9d55565b71b30462
Luzi82 <luzi82@gmail.com>
2014-08-02 14:01:55 +0800

    add gclient

--- prepare.sh
+++ prepare.sh
@@ -8,6 +8,10 @@
 wget http://dl.google.com/android/ndk/android-ndk32-r10-linux-x86_64.tar.bz2
 tar -xjf android-ndk32-r10-linux-x86_64.tar.bz2
 
+# Compile v8 require gclient
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=`pwd`/depot_tools:"$PATH"
+
 # From http://code.google.com/p/v8/wiki/BuildingWithGYP
 git clone git://github.com/v8/v8.git
 cd v8
